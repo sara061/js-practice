0e6a5038c8f2d36f1a0ad6a2a369679a071debb5
Saiyora Rajabova <rsaiyora@gmail.com>
2022-07-19 05:56:51 +0300

    docs:create function

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let money = +prompt("Ваш бюджет на  месяц?", '');
+    time = prompt('Введите дату в формате YYYY-MM-DD' , '');
+
+    let appData = {
+            budget: money,
+            expenses: {},
+            optionalExpenses: {},
+            income: [],
+            timeData: time,
+            saving: false
+    };
+
+    for(let i = 0; i < 2; i++) {
+        
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+        
+            if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+                console.log("done");
+                appData.expenses[a] = b;
+        } else{}
+        
+    };
+    appData.moneyPerDay = appData.budget / 30;
+
+    alert("Ежемесячный бюджет: " + appData.moneyPerDay);
+
+    if(appData.moneyPerDay < 100){
+        console.log('Минимальный уровень достатка')
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console,log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('Высокий уровень достатка');
+    } else {
+        console.log('Произошла ошибка');
+    }
